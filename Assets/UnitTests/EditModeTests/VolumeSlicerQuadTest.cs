a4b785a79f6ada846f1844412831c56ef8519b19
Vitaly Popuzin <popuzin@gmail.com>
2019-04-14 10:14:23 +0300

    Add all test on sliced triangles count for Quad

---
+++ Assets/UnitTests/EditModeTests/VolumeSlicerQuadTest.cs
@@ -0,0 +1,159 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+public class VolumeSlicerQuadTest
+{
+  private VolumeSlicer slicer;
+
+  [SetUp]
+  public void SetUp() => slicer = new VolumeSlicer(0f);
+
+  private void AssertSlicedTrianglesCount(int expected, GameObject objectToSlice)
+  {
+    slicer.Init(objectToSlice);
+    Assert.AreEqual(expected, slicer.MakeSlice().Count);
+  }
+
+  #region ASSERT TRIANGLES COUNT
+
+  #region ZERO TRIANGLES RETURNED
+
+  [Test]
+  public void SliceWithoutInit_ReturnsZeroTriangles() => Assert.AreEqual(0, slicer.MakeSlice().Count);
+
+  [Test]
+  public void SliceNull_ReturnsZeroTriangles() => AssertSlicedTrianglesCount(0, null);
+
+  [Test]
+  public void SliceEmptyGameObject_ReturnsZeroTriangles() => AssertSlicedTrianglesCount(0, new GameObject());
+
+  [Test]
+  public void SliceGameObjectWitEmptyMeshFilter_ReturnsZeroTriangles()
+  {
+    var go = new GameObject();
+    go.AddComponent<MeshFilter>();
+    AssertSlicedTrianglesCount(0, go);
+  }
+
+  [Test]
+  public void SliceBelowQuad_ReturnsZeroTriangles()
+  {
+    slicer = new VolumeSlicer(-1f);
+    AssertSlicedTrianglesCount(0, GameObject.CreatePrimitive(PrimitiveType.Quad));
+  }
+
+  [Test]
+  public void OnBottomSliceOfQuad_ReturnsZeroTriangles()
+  {
+    slicer = new VolumeSlicer(-0.5f);
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+    var mesh = go.GetComponent<MeshFilter>().sharedMesh;
+
+    AssertSlicedTrianglesCount(0, go);
+  }
+
+  #endregion
+
+  #region ALL TRIANGLES RETURNED  
+
+  [Test]
+  public void SliceUnderQuad_ReturnsAllTriangles()
+  {
+    slicer = new VolumeSlicer(1f);
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+    var mesh = go.GetComponent<MeshFilter>().sharedMesh;
+    AssertSlicedTrianglesCount(mesh.triangles.Length / 3, go);
+  }
+
+  [Test]
+  public void InPlaneSliceOfQuad_ReturnsAllTriangles()
+  {
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+    var mesh = go.GetComponent<MeshFilter>().sharedMesh;
+
+    go.transform.Rotate(Vector3.right, 90);
+
+    AssertSlicedTrianglesCount(mesh.triangles.Length / 3, go);
+  }
+
+  [Test]
+  public void OnTopSliceOfQuad_ReturnsAllTriangles()
+  {
+    slicer = new VolumeSlicer(0.5f);
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+    var mesh = go.GetComponent<MeshFilter>().sharedMesh;
+
+    AssertSlicedTrianglesCount(mesh.triangles.Length / 3, go);
+  }
+
+  #endregion
+
+  [TestCase(0.49f)]
+  [TestCase(0.25f)]
+  [TestCase(0)]
+  [TestCase(-0.49f)]
+  [TestCase(-0.25f)]
+  public void SliceQuadOnTriangles_ReturnsTwoTriangles(float slicerShift)
+  {
+    slicer = new VolumeSlicer(slicerShift);
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+    AssertSlicedTrianglesCount(2, go);
+  }
+
+  [Test]
+  public void HalfOfRotatedQuadSlicedByEdge_ReturnsOneTriangle()
+  {
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+    go.transform.Rotate(Vector3.forward, -45);
+
+    AssertSlicedTrianglesCount(1, go);
+  }
+
+  [Test]
+  public void SliceOneLastTriangleInRotatedQuad_ReturnsOneTriangle()
+  {
+    slicer = new VolumeSlicer(-0.25f);
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+    go.transform.Rotate(Vector3.forward, -45);
+
+    AssertSlicedTrianglesCount(1, go);
+  }
+
+  [Test]
+  public void SliceOneTriangleInRotatedQuad_ReturnsOneTrianglePlusRemainedBelow()
+  {
+    slicer = new VolumeSlicer(0.25f);
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+    go.transform.Rotate(Vector3.forward, -45);
+
+    AssertSlicedTrianglesCount(2, go);
+  }
+
+  [Test]
+  public void SliceRotatedQuadOnHalf_ReturnsTwoTriangles()
+  {
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+    go.transform.Rotate(Vector3.forward, 45);
+
+    AssertSlicedTrianglesCount(2, go);
+  }
+  
+  [TestCase(0.25f)]
+  [TestCase(-0.25f)]
+  public void SliceTwoTrianglesInRotatedQuad_ReturnsTwoTriangles(float slicerShift)
+  {
+    slicer = new VolumeSlicer(slicerShift);
+    var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+    go.transform.Rotate(Vector3.forward, 45);
+
+    AssertSlicedTrianglesCount(2, go);
+  }
+
+  #endregion
+}
--- Assets/_Scripts/VolumeSlicer.cs
+++ Assets/_Scripts/VolumeSlicer.cs
@@ -6,50 +6,65 @@ public class VolumeSlicer
 {
   private const float EPS = 1E-6f;
   private readonly float _slicerShift;
-
+  
+  private Mesh _mesh;
+  private Transform _meshTransform;
+  
+  private int[] _meshTriangles = new int[0];
+  private Vector3[] _meshVertices;
+  
   public VolumeSlicer(float slicerShift) => _slicerShift = slicerShift;
 
-  public List<Triangle> MakeSlice(GameObject objectToSlice)
+  public void Init(GameObject objectToSlice)
   {
-    if (objectToSlice == null || objectToSlice.GetComponent<MeshFilter>() == null)
-      return new List<Triangle>();
-
-    var mesh = objectToSlice.GetComponent<MeshFilter>().sharedMesh;
-    var slicedTris = new List<Triangle>();
-
-    var meshTransform = objectToSlice.transform;
-    var meshTriangles = mesh.triangles;
-    var meshVertices = mesh.vertices;
-
-    for (var i = 0; i < meshTriangles.Length; i += 3)
+    if (objectToSlice == null || objectToSlice.GetComponent<MeshFilter>() == null ||
+        objectToSlice.GetComponent<MeshFilter>().sharedMesh == null) return;
+    
+    _mesh = objectToSlice.GetComponent<MeshFilter>().sharedMesh;
+    _meshTransform = objectToSlice.transform;
+    _meshTriangles = _mesh.triangles;
+    _meshVertices = _mesh.vertices;
+  }
+  
+  public List<Triangle> MakeSlice()
+  {    
+    var slicedTris = new List<Triangle>();   
+    
+    for (var i = 0; i < _meshTriangles.Length; i += 3)
     {
       var vertices = new Vector3[3];
       var vertDistToPlane = new float[3];
 
       for (var j = 0; j < 3; j++)
       {
-        vertices[j] = meshTransform.TransformPoint(meshVertices[meshTriangles[i + j]]);
-        vertDistToPlane[j] = vertices[j].y - (meshTransform.position.y + _slicerShift);
+        vertices[j] = _meshTransform.TransformPoint(_meshVertices[_meshTriangles[i + j]]);
+        vertDistToPlane[j] = vertices[j].y - (_meshTransform.position.y + _slicerShift);
       }
 
       if (vertDistToPlane[0] < -EPS && vertDistToPlane[1] < -EPS && vertDistToPlane[2] < -EPS)
-        slicedTris.Add(new Triangle(vertices));
-
-      if (Math.Abs(vertDistToPlane[0]) <= EPS && Math.Abs(vertDistToPlane[1]) <= EPS &&
-          Math.Abs(vertDistToPlane[2]) <= EPS)
       {
-        slicedTris.Add(new Triangle(vertices));
-        continue;
+        slicedTris.Add(new Triangle(vertices));        
       }
-
-      if (Math.Abs(vertDistToPlane[0]) <= EPS || Math.Abs(vertDistToPlane[1]) <= EPS ||
+      else if (Math.Abs(vertDistToPlane[0]) <= EPS && Math.Abs(vertDistToPlane[1]) <= EPS &&
           Math.Abs(vertDistToPlane[2]) <= EPS)
       {
-        if (vertDistToPlane[0] * vertDistToPlane[1] < -EPS) continue;
-        if (vertDistToPlane[1] * vertDistToPlane[2] < -EPS) continue;
-        if (vertDistToPlane[2] * vertDistToPlane[0] < -EPS) continue;
+        slicedTris.Add(new Triangle(vertices));        
+      }
+      else if (Math.Abs(vertDistToPlane[0]) <= EPS || Math.Abs(vertDistToPlane[1]) <= EPS ||
+          Math.Abs(vertDistToPlane[2]) <= EPS)
+      {
+       
+//        if (vertDistToPlane[0] * vertDistToPlane[1] < -EPS) continue;
+//        if (vertDistToPlane[1] * vertDistToPlane[2] < -EPS) continue;
+//        if (vertDistToPlane[2] * vertDistToPlane[0] < -EPS) continue;
 
         if (vertDistToPlane[0] < -EPS || vertDistToPlane[1] < -EPS || vertDistToPlane[2] < -EPS)
+          slicedTris.Add(new Triangle(vertices));                
+      }
+      else if (vertDistToPlane[0] < -EPS || vertDistToPlane[1] < -EPS || vertDistToPlane[2] < -EPS)
+      {
+        if (vertDistToPlane[0] * vertDistToPlane[1] < -EPS || vertDistToPlane[1] * vertDistToPlane[2] < -EPS 
+            || vertDistToPlane[2] * vertDistToPlane[0] < -EPS) 
           slicedTris.Add(new Triangle(vertices));
       }
     }
